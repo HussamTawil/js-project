a27b2661716c8d60a6235631fcffaae7af03ba33
HussamTawil <hossam145@hotmail.com>
2023-01-06 19:12:14 +0200

    third_comm

---
+++ jsfile.js
@@ -0,0 +1,20 @@
+let display = document.getElementById('display');
+let buttons = Array.from( document.getElementsByClassName('button'));
+
+buttons.map( button =>{
+    button.addEventListener('click',(e)=>{
+        switch(e.target.innerText){
+            case'c':
+                display.innerText='';
+                break;
+            case'⇚':
+                if(display.innerText){
+                    display.innerText= display.innerText.slice(0,-1);
+                    }
+                    break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+
+    });
+});
