5e4c0cbe64d83007e28f0e678e1eea5f3edfd47f
HussamTawil <hossam145@hotmail.com>
2023-01-06 19:34:11 +0200

    finshed

--- jsfile.js
+++ jsfile.js
@@ -12,6 +12,13 @@ buttons.map( button =>{
                     display.innerText= display.innerText.slice(0,-1);
                     }
                     break;
+            case'=':
+                try{
+                    display.innerText= eval(display.innerText);
+                }catch{
+                    display.innerText='Error !';
+                }
+                    break;
             default:
                 display.innerText += e.target.innerText;
         }
